3ac24f6794c4b7ecb637451a495cca5b0a56894f
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-07-31 01:01:41 +0200

    Updated modpatch typing

--- server/custom-endpoints/modpatch.ts
+++ server/custom-endpoints/modpatch.ts
@@ -1,49 +1,16 @@
-import path from 'path';
-import fs from 'fs';
-import type { ModdedSpeciesData } from "../../sim/dex-species";
+import type { ModdedLearnsetDataTable, ModdedSpeciesData, ModdedSpeciesFormatsDataTable } from "../../sim/dex-species";
+import type { ModdedMoveDataTable } from "../../sim/dex-moves";
+import type { ModdedItemDataTable } from "../../sim/dex-items";
+import type { ModdedAbilityDataTable } from "../../sim/dex-abilities";
+import type { ModdedTypeDataTable } from "../../sim/dex-data";
 
 export class ModPatch {
 	parentMod: string | null = null;
 	pokedex: { [speciesid: string]: ModdedSpeciesData } = {};
-	abilities = new Map<string, any>();
-	moves = new Map<string, any>();
-	items = new Map<string, any>();
-	typechart = new Map<string, any>();
-	learnsets = new Map<string, any>();
-	formatsData = new Map<string, any>();
-
-	static fromMod(modName: string): ModPatch {
-		const modPath = path.resolve(__dirname, '../../../data/mods', modName);
-		console.log(modPath);
-		function tryImport(file: string) {
-			const fullPath = path.join(modPath, file);
-			if (fs.existsSync(fullPath)) {
-				return require(fullPath);
-			}
-			return null;
-		}
-
-		const modPatch = new ModPatch();
-
-		if (!fs.existsSync(modPath)) {
-			console.log("Mod not found");
-			return modPatch;
-		}
-
-		modPatch.pokedex = tryImport('pokedex.ts')?.Pokedex || {};
-		modPatch.learnsets = tryImport('learnsets.ts')?.Learnsets || {};
-		modPatch.formatsData = tryImport('formats-data.ts')?.FormatsData || {};
-		modPatch.typechart = tryImport('typechart.ts')?.Typechart || {};
-		modPatch.items = tryImport('items.ts')?.Items || {};
-		modPatch.moves = tryImport('moves.ts')?.Moves || {};
-		modPatch.abilities = tryImport('abilities.ts')?.Abilities || {};
-
-		const scripts = tryImport('scripts.ts')?.Scripts;
-		modPatch.parentMod = scripts?.inherit || null;
-		if (scripts?.init) {
-			scripts.init(modPatch);
-		}
-
-		return modPatch;
-	}
+	abilities: ModdedAbilityDataTable = {};
+	moves: ModdedMoveDataTable = {};
+	items: ModdedItemDataTable = {};
+	typechart: ModdedTypeDataTable = {};
+	learnsets: ModdedLearnsetDataTable = {};
+	formatsData: ModdedSpeciesFormatsDataTable = {};
 }
