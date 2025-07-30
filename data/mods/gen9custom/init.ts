d5fbfef8efaed0c9655def9ea7d369ee7f32a9d9
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-07-31 01:11:10 +0200

    Added gen9custom change template files.

--- data/mods/gen9custom/init.ts
+++ data/mods/gen9custom/init.ts
@@ -2,11 +2,17 @@ import { ModPatch } from "../../../server/custom-endpoints/modpatch";
 import { FormatsData } from "./formats-data";
 import { Pokedex } from "./pokedex";
 import { Scripts } from "./scripts";
+import { Items } from "./items";
+import { TypeChart } from "./typechart";
+import { Moves } from "./moves";
 
 // Generates cache file for this custom mode.
 const modPatch = new ModPatch();
 modPatch.pokedex = Pokedex;
 modPatch.formatsData = FormatsData;
+modPatch.moves = Moves;
+modPatch.typechart = TypeChart;
+modPatch.items = Items;
 
 if (Scripts?.init) {
 	Scripts.init(modPatch);
