52c78c564bc16dfb518419b7b5907f787c0bff95
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-08-06 18:00:31 +0200

    Made Bug neutral to Fairy in gen9natdexcustom

--- data/mods/gen9custom/typechart.ts
+++ data/mods/gen9custom/typechart.ts
@@ -1,4 +1,10 @@
 import type { ModdedTypeDataTable } from "../../../sim/dex-data";
 
 export const TypeChart: ModdedTypeDataTable = {
+	fairy: {
+		inherit: true,
+		damageTaken: {
+			Bug: 0,
+		},
+	},
 };
