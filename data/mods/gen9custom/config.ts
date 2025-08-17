91b52b3e3de6bb538dc7e655e0d086c8052def5b
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-08-18 01:01:57 +0200

    tuned config

--- data/mods/gen9custom/config.ts
+++ data/mods/gen9custom/config.ts
@@ -1,8 +1,8 @@
 export const ModConfig = {
 	// Restricted limit per team
-	maxRestricted: 0, // Hard cap of both; if 1, use either or.
+	maxRestricted: 1, // Hard cap of both; if 1, use either or.
 	maxUber: 1,
-	maxAG: 1,
+	maxAG: 0,
 	// Team size
 	uberTeamSize: 5,
 	agTeamSize: 4,
