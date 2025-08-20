1d763e168cdd37462feded330e6e400726a70355
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-08-20 17:04:14 +0200

    Update rulesets.ts

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -37,7 +37,8 @@ export const Rulesets: import('../sim/dex-formats').FormatDataTable = {
 				// length limit implementation and copy-pasting the result here.
 				if (set.name.length > 18) { problems.push(`Nickname "${set.name}" too long (should be 18 characters or fewer)`); }
 				if (set.ability !== 'Illusion') {
-					if (nameSpecies.id !== setSpecies.id) {
+					if (nameSpecies.name !== species.name &&
+						nameSpecies.name !== species.baseSpecies && ruleTable.has('nicknameclause')) {
 						problems.push(`${set.name} must not be nicknamed a different Pokémon species than what it actually is.`);
 					}
 					continue; // Done checking for this set.
