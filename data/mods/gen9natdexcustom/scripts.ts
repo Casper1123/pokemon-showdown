38273825cc5fbc5f23f7cf8bc450906ed7e5dc2a
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-09-13 19:49:25 +0200

    fixed type error :(

--- data/mods/gen9natdexcustom/scripts.ts
+++ data/mods/gen9natdexcustom/scripts.ts
@@ -10,11 +10,37 @@ export const Scripts: ModdedBattleScriptsData = {
 		isGrounded(negateImmunity) {
 			if ('gravity' in this.battle.field.pseudoWeather ||
 				'spatialdistortions' in this.battle.field.pseudoWeather) return true;
-			return super.isGrounded(negateImmunity);
+			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
+			if ('smackdown' in this.volatiles) return true;
+			const item = (this.ignoringItem() ? '' : this.item);
+			if (item === 'ironball') return true;
+			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
+			if (!negateImmunity && this.hasType('Flying') && !(this.hasType('???') && 'roost' in this.volatiles)) return false;
+			if (this.hasAbility('levitate') && !this.battle.suppressingAbility(this)) return null;
+			if ('magnetrise' in this.volatiles) return false;
+			if ('telekinesis' in this.volatiles) return false;
+			return item !== 'airballoon';
 		},
 		ignoringAbility() {
 			if (this.abilityState.suppressed) return true;
-			return super.ignoringAbility();
+
+			if (this.battle.gen >= 5 && !this.isActive) return true;
+
+			// Certain Abilities won't activate while Transformed, even if they ordinarily couldn't be suppressed (e.g. Disguise)
+			if (this.getAbility().flags['notransform'] && this.transformed) return true;
+			if (this.getAbility().flags['cantsuppress']) return false;
+			if (this.volatiles['gastroacid']) return true;
+
+			// Check if any active pokemon have the ability Neutralizing Gas
+			if (this.hasItem('Ability Shield') || this.ability === ('neutralizinggas' as ID)) return false;
+			for (const pokemon of this.battle.getAllActive()) {
+				// can't use hasAbility because it would lead to infinite recursion
+				if (pokemon.ability === ('neutralizinggas' as ID) && !pokemon.volatiles['gastroacid'] &&
+					!pokemon.transformed && !pokemon.abilityState.ending && !this.volatiles['commanding']) {
+					return true;
+				}
+			}
+			return false;
 		},
 	},
 };
