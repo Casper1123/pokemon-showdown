550fa2690054979081a1f3216c4040228548ae25
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-10-08 00:52:02 +0200

    dude I'm coping so bad rn

--- data/mods/gen9natdexcustom/scripts.ts
+++ data/mods/gen9natdexcustom/scripts.ts
@@ -1,7 +1,10 @@
+import {applyChanges} from "./learnset_changes";
+
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen9',
 	gen: 9,
 	init() {
+		applyChanges(this);
 	},
 	pokemon: {
 		isGrounded(negateImmunity) {
