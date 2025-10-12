3b995ab64ca75654a8a070a9056c90b260f154d9
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2025-10-12 02:26:34 +0200

    mod-highlighted tiering for client sorting

--- data/mods/gen9natdexcustom/formats-data.ts
+++ data/mods/gen9natdexcustom/formats-data.ts
@@ -14,9 +14,468 @@ export const FormatsData: import('../../../sim/dex-species').SpeciesFormatsDataT
 		tier: "Illegal",
 		natDexTier: "OU",
 	},
+
+	// region modded pokemon
+	// region gen1
+	gengar: {
+		tier: "RU",
+		doublesTier: "DMOD",
+		natDexTier: "MOD",
+	},
+	pidgeotmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	slowbromega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	moltresgalar: {
+		tier: "UUBL",
+		doublesTier: "DMOD",
+		natDexTier: "MOD",
+	},
+	// endregion
+	// region gen2
+	ampharos: {
+		tier: "ZU",
+		doublesTier: "DMOD",
+		natDexTier: "MOD",
+	},
+	ampharosmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	celebi: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	houndoommega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	raikou: {
+		tier: "NU",
+		doublesTier: "DMOD",
+		natDexTier: "MOD",
+	},
+	steelixmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	suicune: {
+		tier: "NUBL",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	typhlosion: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	typhlosionhisui: {
+		tier: "PU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	politoed: {
+		tier: "NUBL",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	crobat: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	// endregion
+	// region gen3
+	absolmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	aggron: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	aggronmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	altariamega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	banettemega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	breloom: {
+		tier: "NU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	cameruptmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	deoxys: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	deoxysdefense: {
+		tier: "NUBL",
+		doublesTier: "DMOD",
+		natDexTier: "MOD",
+	},
+	flygon: {
+		tier: "NU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	manectricmega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	metagross: {
+		tier: "UU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	sceptile: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	sceptilemega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	sharpedomega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	// endregion
+	// region gen4
 	garchompmega: {
 		isNonstandard: "Past",
 		tier: "Illegal",
-		natDexTier: "OU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
 	},
+	hippowdon: {
+		tier: "RU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	empoleon: {
+		tier: "RU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	shaymin: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	glaceon: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	dialga: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	dialgaorigin: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	palkia: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	palkiaorigin: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	giratina: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	giratinaorigin: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	toxicroak: {
+		tier: "NU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	rotom: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	rotomheat: {
+		tier: "PU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	rotomwash: {
+		tier: "UU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	rotomfrost: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	rotomfan: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	rotommow: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	gallade: {
+		tier: "RU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	gallademega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	// endregion
+	// region gen5
+	serperior: {
+		tier: "RUBL",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	scolipede: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	conkeldurr: {
+		tier: "UU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	haxorus: {
+		tier: "RUBL",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	durant: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	gigalith: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	cobalion: {
+		tier: "UU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	terrakion: {
+		tier: "NU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	virizion: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	zoroark: {
+		tier: "PU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	zoroarkhisui: {
+		tier: "RUBL",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	swanna: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	samurott: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	reuniclus: {
+		tier: "RU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	krookodile: {
+		tier: "RU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	cofagrigus: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	chandelure: {
+		tier: "NU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	audino: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	audinomega: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	beartic: {
+		tier: "ZU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	beheeyem: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	musharna: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	kyuremblack: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	kyuremwhite: {
+		tier: "Uber",
+		doublesTier: "DMOD Uber",
+		natDexTier: "MOD Uber",
+	},
+	darmanitan: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	keldeo: {
+		tier: "UU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	victini: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	// endregion
+	// region gen6
+	noivern: {
+		tier: "RU",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	// endregion
+	// region gen7
+	golisopod: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	nihilego: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	xurkitree: {
+		isNonstandard: "Past",
+		tier: "Illegal",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	necrozma: {
+		tier: "NUBL",
+		natDexTier: "MOD",
+		doublesTier: "DMOD",
+	},
+	// endregion
+	// region gen8
+
+	// endregion
+	// region gen9
+
+	// endregion
+	// endregion
 };
